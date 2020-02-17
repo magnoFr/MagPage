ee8894bc3ddec56554afe52f4b540d33b3b0e202
Magno Augusto Ferreira Ruivo <magnoaugustofr@gmail.com>
2020-02-16 23:22:04 -0300

    mag

---
+++ MagTestTests/MagTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  MagTestTests.swift
+//  MagTestTests
+//
+//  Created by Mag on 13/02/20.
+//  Copyright © 2020 Mag. All rights reserved.
+//
+
+import XCTest
+@testable import MagTest
+
+class MagTestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
